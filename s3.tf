5788c144bb75996f7f30037577022c852c8e5e51
jeaggrey <jeaggrey@gmail.com>
2023-03-14 21:38:26 -0400

    Pushing s3 bucket Branch to Remote

---
+++ s3.tf
@@ -0,0 +1,29 @@
+resource "aws_kms_key" "joetechkey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 30
+}
+
+resource "aws_s3_bucket" "joetech" {
+   acl = "private"
+   bucket = "bootcamp30"  
+    object_lock_enabled = true
+    
+     versioning {
+    enabled = true
+  }
+    
+    # tags = {
+    #   "Name" = bootcamp30-${ra}
+    # }
+  
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.joetech.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.joetechkey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
